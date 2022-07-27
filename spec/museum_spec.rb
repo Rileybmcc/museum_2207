313accbfb0726d91df8fb6edbafe7a36835e4ed8
Riley McCullough <rileybmccullough@gmail.com>
2022-07-27 10:37:39 -0600

    Fix: add global symbols where missing

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-    @dmns.add_exhibit(gems_and_minerals)
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
 
@@ -28,9 +27,9 @@ RSpec.describe Museum do
   end
 
   xit 'can add exhibits' do
-    @dmns.add_exhibit(gems_and_minerals)
-    @dmns.add_exhibit(dead_sea_scrolls)
-    @dmns.add_exhibit(imax)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
 
     expect(@dmns.exhibits.count).to eq(3)
   end
@@ -41,7 +40,7 @@ RSpec.describe Museum do
     @patron_1.add_interest("Gems and Minerals")
     @patron_2.add_interest("IMAX")
 
-    expect(@dmns.recommend_exhibits(patron_1).count).to eq(2)
-    expect(@dmns.recommend_exhibits(patron_2).count).to eq(1)
+    expect(@dmns.recommend_exhibits(@patron_1).count).to eq(2)
+    expect(@dmns.recommend_exhibits(@patron_2).count).to eq(1)
   end
 end
